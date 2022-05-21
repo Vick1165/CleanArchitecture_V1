a44bae7df061a3b762ca493d4448112369b98130
Balaji Kumar <vickykumar538@gmail.com>
2022-05-21 17:53:22 +0530

    added repository

---
+++ CleanArchitecture.Core/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Core.Repositories.Base;
+
+namespace CleanArchitecture.Core.Repositories;
+
+public interface IEmployeeRepository : IRepository<Employee>
+{
+    Task<IEnumerable<Employee>> GetEmployeeByLastName(string lastname);
+}
---
+++ CleanArchitecture.Infrastructure/Repositories/EmployeeRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Core.Entities;
+using CleanArchitecture.Core.Repositories;
+using CleanArchitecture.Infrastructure.Data;
+using CleanArchitecture.Infrastructure.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Infrastructure.Repositories;
+
+public class EmployeeRepository : Repository<Employee>, IEmployeeRepository
+{
+    private readonly EmployeeContext _context;
+
+    public EmployeeRepository(EmployeeContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Employee>> GetEmployeeByLastName(string lastname)
+    {
+        return await _context.Employees.Where(m => m.LastName == lastname).ToListAsync();
+    }
+
+
+}
